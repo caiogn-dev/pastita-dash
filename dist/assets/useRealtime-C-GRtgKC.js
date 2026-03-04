49d1c97db760f3e90617864d46a73c218a42d44d
Pastita Dev <dev@pastita.com>
2026-03-04 16:11:56 -0300

    ok

---
+++ dist/assets/useRealtime-C-GRtgKC.js
@@ -0,0 +1,1 @@
+import"./vendor-chakra-3uRsBA2g.js";import{r}from"./vendor-react-DCoebqfJ.js";import"./feature-automation-DLcyWime.js";r.createContext(null);
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,7 @@ import { LoginResponse, User } from '../types';
 export const authService = {
   login: async (email: string, password: string): Promise<LoginResponse> => {
     const response = await api.post<LoginResponse>('/auth/login/', {
-      username: email,
+      email: email,
       password,
     });
     return response.data;
